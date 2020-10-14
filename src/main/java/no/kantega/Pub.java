c61345e6ef9c72de2b1be88f3ffa51ff47279d49
Ove Gram Nipen <ove@nipen.no>
2020-10-14 10:27:55 +0200

    replace if/then/else with drink price

--- src/main/java/no/kantega/Pub.java
+++ src/main/java/no/kantega/Pub.java
@@ -9,22 +9,7 @@ public class Pub {
         if (quantity > 2 && (drink == GT || drink == BACARDI_SPECIAL)) {
             throw new RuntimeException("Too many drinks, max 2.");
         }
-        int price;
-        if (drink.equals(ONE_BEER)) {
-            price = drink.getPrice();
-        } else if (drink.equals(ONE_CIDER)) {
-            price = drink.getPrice();
-        }
-        else if (drink.equals(A_PROPER_CIDER)) price = drink.getPrice();
-        else if (drink.equals(GT)) {
-            price = drink.getPrice();
-        }
-        else if (drink.equals(BACARDI_SPECIAL)) {
-            price = drink.getPrice();
-        }
-        else {
-            throw new RuntimeException("No such drink exists");
-        }
+        int price = drink.getPrice();
         if (student && (drink == ONE_CIDER || drink == ONE_BEER || drink == A_PROPER_CIDER)) {
             price = price - price/10;
         }
