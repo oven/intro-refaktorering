38d0b5407aba45edf093b2dc2040e859d38c0338
Ove Gram Nipen <ove@nipen.no>
2020-10-14 10:38:33 +0200

    extract method to apply discount

--- src/main/java/no/kantega/Pub.java
+++ src/main/java/no/kantega/Pub.java
@@ -3,15 +3,18 @@ package no.kantega;
 public class Pub {
 
     public int computeCost(MenuItem drink, boolean student, int quantity) {
-
         if (quantity > 2 && drink.isCocktail) {
             throw new RuntimeException("Too many drinks, max 2.");
         }
+        int price = applyDiscount(drink, student);
+        return price * quantity;
+    }
+
+    private int applyDiscount(MenuItem drink, boolean student) {
         int price = drink.getPrice();
         if (student && !drink.isCocktail) {
             price = price - price/10;
         }
-        return price * quantity;
+        return price;
     }
-
 }
