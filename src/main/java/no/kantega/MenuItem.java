a22da81543ad5fb1c9d711217774628d9a297c41
Ove Gram Nipen <ove@nipen.no>
2020-10-14 11:14:23 +0200

    remove MenuItem.id, since nobody used it

--- src/main/java/no/kantega/MenuItem.java
+++ src/main/java/no/kantega/MenuItem.java
@@ -6,24 +6,21 @@ import static no.kantega.Ingredient.*;
 import static no.kantega.RecipeEntry.add;
 
 public enum MenuItem {
-    ONE_BEER("hansa", 74),
-    ONE_CIDER("grans", 103),
-    A_PROPER_CIDER("strongbow", 110),
-    GT("gt", add(1, GIN), add(1, TONIC_WATER), add(1, GREEN_STUFF)),
-    BACARDI_SPECIAL("bacardi_special", add(0.5, GIN), add(1, RUM), add(1, GRENADINE), add(1, LIME_JUICE));
+    ONE_BEER(74),
+    ONE_CIDER(103),
+    A_PROPER_CIDER(110),
+    GT(add(1, GIN), add(1, TONIC_WATER), add(1, GREEN_STUFF)),
+    BACARDI_SPECIAL(add(0.5, GIN), add(1, RUM), add(1, GRENADINE), add(1, LIME_JUICE));
 
-    public final String id;
     private final int price;
     public final boolean isCocktail;
 
-    MenuItem(String id, RecipeEntry... ingredients) {
-        this.id = id;
+    MenuItem(RecipeEntry... ingredients) {
         this.isCocktail = true;
         this.price = Arrays.stream(ingredients).mapToInt(RecipeEntry::getPrice).sum();
     }
 
-    MenuItem(String id, int price) {
-        this.id = id;
+    MenuItem(int price) {
         this.price = price;
         this.isCocktail = false;
     }
