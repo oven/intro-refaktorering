f1aae4e95ad1fc89581b69a77ead4ba65fb76638
Ove Gram Nipen <ove@nipen.no>
2020-10-14 11:10:21 +0200

    Use recipe for bacardi special as well

--- src/main/java/no/kantega/MenuItem.java
+++ src/main/java/no/kantega/MenuItem.java
@@ -8,7 +8,7 @@ public enum MenuItem {
     ONE_CIDER("grans", 103, false),
     A_PROPER_CIDER("strongbow", 110, false),
     GT("gt", add(1, GIN), add(1, TONIC_WATER), add(1, GREEN_STUFF)),
-    BACARDI_SPECIAL("bacardi_special", GIN.price / 2 + RUM.price + GRENADINE.price + LIME_JUICE.price, true);
+    BACARDI_SPECIAL("bacardi_special", add(0.5, GIN), add(1, RUM), add(1, GRENADINE), add(1, LIME_JUICE));
 
     public final String id;
     private final int price;
