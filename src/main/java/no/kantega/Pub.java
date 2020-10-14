e0601ffb50ec6463f1567106f7cac61fe514dc07
Ove Gram Nipen <ove@nipen.no>
2020-10-14 11:17:13 +0200

    extract method to validate order

--- src/main/java/no/kantega/Pub.java
+++ src/main/java/no/kantega/Pub.java
@@ -3,11 +3,14 @@ package no.kantega;
 public class Pub {
 
     public int computeCost(MenuItem drink, boolean student, int quantity) {
-        if (quantity > 2 && drink.isCocktail) {
-            throw new RuntimeException("Too many drinks, max 2.");
-        }
+        validateOrder(drink, quantity);
         int price = DiscountCalculator.applyDiscount(drink, student);
         return price * quantity;
     }
 
+    private void validateOrder(MenuItem drink, int quantity) {
+        if (quantity > 2 && drink.isCocktail) {
+            throw new RuntimeException("Too many drinks, max 2.");
+        }
+    }
 }
