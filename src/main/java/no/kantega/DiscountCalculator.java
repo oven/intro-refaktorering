de1cf0a47df72b38dc020bf2b9594ce5ba35a939
Ove Gram Nipen <ove@nipen.no>
2020-10-14 10:49:42 +0200

    Introduce student discount calculator

--- src/main/java/no/kantega/DiscountCalculator.java
+++ src/main/java/no/kantega/DiscountCalculator.java
@@ -4,6 +4,7 @@ public abstract class DiscountCalculator {
 
     public static int applyDiscount(MenuItem drink, boolean student) {
         if (drink.isCocktail) return new NoDiscountCalculator().applyDiscount(drink);
+        if (student) return new StudentDiscountCalculator().applyDiscount(drink);
 
         int price = drink.getPrice();
         if (student && !drink.isCocktail) {
@@ -18,6 +19,16 @@ public abstract class DiscountCalculator {
         @Override
         public int applyDiscount(MenuItem drink) {
             return drink.getPrice();
+        }
+    }
+
+    public static class StudentDiscountCalculator extends DiscountCalculator {
+
+        @Override
+        public int applyDiscount(MenuItem drink) {
+            int price = drink.getPrice();
+            price = price - price / 10;
+            return price;
         }
     }
 }
