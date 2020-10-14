dc2047f0dfe521f8fc97e83d3c3f3aabbeae39b2
Ove Gram Nipen <ove@nipen.no>
2020-10-14 10:46:03 +0200

    introduce discount strategies

--- src/main/java/no/kantega/DiscountCalculator.java
+++ src/main/java/no/kantega/DiscountCalculator.java
@@ -1,12 +1,23 @@
 package no.kantega;
 
-public class DiscountCalculator {
+public abstract class DiscountCalculator {
 
     public static int applyDiscount(MenuItem drink, boolean student) {
+        if (drink.isCocktail) return new NoDiscountCalculator().applyDiscount(drink);
+
         int price = drink.getPrice();
         if (student && !drink.isCocktail) {
-            price = price - price/10;
+            price = price - price / 10;
         }
         return price;
+    }
+
+    public abstract int applyDiscount(MenuItem drink);
+
+    public static class NoDiscountCalculator extends DiscountCalculator {
+        @Override
+        public int applyDiscount(MenuItem drink) {
+            return drink.getPrice();
+        }
     }
 }
